29fc8bbb128e26162e3bbeaa1b5ddaa17633e274
vibhormalik-ia <vibhor.malik@indiaaccelerator.co>
2026-07-22 23:29:36 +0530

    Add deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/heena-birthday/',
   plugins: [tailwindcss()],
   build: {
     chunkSizeWarningLimit: 1400,
